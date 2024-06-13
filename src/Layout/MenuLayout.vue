10c0368bd32457b0eb25eee4589b1d264381b957
mjpark07 <ryuback0826@naver.com>
2024-06-13 22:22:42 +0900

    레이아웃 추가 및 수정

---
+++ src/Layout/MenuLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="menu-layout">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MenuLayout'
+  };
+  </script>
+  
+  <style>
+  .workspace-container {
+    display: grid;
+    flex-direction: column;
+  }
+  .workspace-header {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+  .workspace-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 12px 10px;
+    width: 100%;
+    border-bottom: 1px solid #ccc; 
+  }
+  .document-list {
+    display: grid;
+  }
+  </style>
---
+++ src/Layout/ServeLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="serve-layout">
+    </div>
+    </template>
+    
+    <script>
+    export default {
+      name: 'ServeLayout'
+    };
+    </script>
+    
+    <style>
+    .site-layout {
+      display: flex;
+      flex-direction: column;
+      height: 679px;
+    }
+    
+    .site-title {
+      display: grid;
+    }
+    
+    </style>
